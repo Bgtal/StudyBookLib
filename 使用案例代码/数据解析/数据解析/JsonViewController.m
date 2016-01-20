5127d9a125bf5a0420aaaaaca0f8a50e347ddcc4
SSNB <Blq_ssnb@outlook.com>
2016-01-20 19:16:41 +0800

    添加了数据解析中的JSON解析和Xml解析的原生写法

--- 使用案例代码/数据解析/数据解析/JsonViewController.m
+++ 使用案例代码/数据解析/数据解析/JsonViewController.m
@@ -9,29 +9,63 @@
 #import "JsonViewController.h"
 
 @interface JsonViewController ()
-
+@property (nonatomic,strong)  NSString *jsonPath;
+@property (strong, nonatomic) IBOutlet UILabel *showLab;
+@property (strong, nonatomic) IBOutlet UILabel *json;
 @end
 
 @implementation JsonViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    NSString *st = [[NSString alloc]initWithContentsOfFile:self.jsonPath encoding:(NSUTF8StringEncoding) error:nil];
+    self.json.text=[NSString stringWithFormat:@"Json原文：\n%@",st];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+- (IBAction)jiexi:(id)sender {
+    NSData *data = [NSData dataWithContentsOfFile:self.jsonPath];
+    //接收原则:{}使用字典;[]使用数组
+    NSError *err = nil;
+    NSDictionary *dic = [NSJSONSerialization JSONObjectWithData:data options:(NSJSONReadingMutableContainers) error:&err];
+    if (err) {
+        self.showLab.text =[err description];
+        return;
+    }
+    
+    NSMutableString *str = [[NSMutableString alloc]initWithCapacity:100];
+    [str appendString:@"Json解析:\n{\n"];
+    
+    NSInteger FeelsLikeC = [[dic objectForKey:@"FeelsLikeC"] integerValue];
+    [str appendString:[NSString stringWithFormat:@"FeelsLikeC:%ld,\n",FeelsLikeC]];
+    
+    NSString *temp_c = [dic objectForKey:@"temp_c"];
+    [str appendString:@"temp_c:"];
+    [str appendString:temp_c];
+    [str appendString:@",\n"];
+    
+    NSArray *arr = [dic objectForKey:@"request"];
+    [str appendString:@"request:\n[\n"];
+    
+    NSDictionary *dic2 = arr[0];
+    [str appendString:@"{\nquery:"];
+    NSString *query = [dic2 objectForKey:@"query"];
+    [str appendString:query];
+    
+    [str appendString:@"\n}\n]\n}"];
+    
+    self.showLab.text = str;
+    
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- ( NSString *)jsonPath {
+	if(_jsonPath == nil) {
+        _jsonPath = [[NSBundle mainBundle]pathForResource:@"mJson" ofType:@"json"];
+	}
+	return _jsonPath;
 }
-*/
 
 @end
--- 使用案例代码/数据解析/数据解析/XmlViewController.m
+++ 使用案例代码/数据解析/数据解析/XmlViewController.m
@@ -8,30 +8,95 @@
 
 #import "XmlViewController.h"
 
-@interface XmlViewController ()
-
+@interface XmlViewController ()<NSXMLParserDelegate>
+@property (nonatomic,strong) NSString *xmlPath;
+@property (strong, nonatomic) IBOutlet UILabel *xmlLab;
+@property (strong, nonatomic) IBOutlet UILabel *showLab;
+@property (nonatomic,strong) NSMutableString *xmlStr;
 @end
 
 @implementation XmlViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    NSString *str = [[NSString alloc]initWithContentsOfFile:self.xmlPath encoding:(NSUTF8StringEncoding) error:nil];
+    self.xmlLab.text = [NSString stringWithFormat:@"xml原文:%@",str];
 }
+
+- (IBAction)jiexi:(id)sender {
+    NSData *data = [NSData dataWithContentsOfFile:self.xmlPath];
+    
+    NSXMLParser *xmlParser = [[NSXMLParser alloc]initWithData:data];
+    xmlParser.delegate = self;
+    [xmlParser parse];
+    
+}
+-(void)parserDidStartDocument:(NSXMLParser *)parser{
+    NSLog(@"开始解析");
+}
+
+-(void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName attributes:(NSDictionary<NSString *,NSString *> *)attributeDict{
+    [self.xmlStr appendString:[NSString stringWithFormat:@"<%@>",elementName]];
+}
+
+-(void)parser:(NSXMLParser *)parser foundCharacters:(nonnull NSString *)string{
+    [self.xmlStr appendString:string];
+}
+
+-(void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName{
+    [self.xmlStr appendString:[NSString stringWithFormat:@"</%@>",elementName]];
+}
+
+-(void)parserDidEndDocument:(NSXMLParser *)parser{
+    NSLog(@"%@",self.xmlStr);
+    self.showLab.text = self.xmlStr;
+}
+/**
+ *  @author ssnb, 16-01-20 16:01:27
+ *
+ *  解析错误时候返回
+ *
+ */
+-(void)parser:(NSXMLParser *)parser parseErrorOccurred:(NSError *)parseError{
+    if (parseError) {
+        NSLog(@"dd");
+        [parser abortParsing];
+        
+        self.showLab.text = self.xmlStr;
+    }
+}
+/**
+ *  @author ssnb, 16-01-20 16:01:48
+ *
+ *  验证的时候错误
+ *
+ */
+-(void)parser:(NSXMLParser *)parser validationErrorOccurred:(NSError *)validationError{
+    if (validationError) {
+        NSLog(@"xxx");
+        [parser abortParsing];
+        
+//        self.showLab.text = self.xmlStr;
+    }
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (NSString *)xmlPath {
+	if(_xmlPath == nil) {
+		_xmlPath = [[NSBundle mainBundle] pathForResource:@"mXml" ofType:@"xml"];
+	}
+	return _xmlPath;
 }
-*/
+
+- (NSMutableString *)xmlStr {
+	if(_xmlStr == nil) {
+		_xmlStr = [[NSMutableString alloc] init];
+	}
+	return _xmlStr;
+}
 
 @end
